8862b340dc4c9ffd820f0a11ea3fccbf72959fb4
Alessandro Pandini <alessandro.pandini@gmail.com>
2019-10-05 20:51:50 +0100

    add lr to LIGFX

--- LIGFX.py
+++ LIGFX.py
@@ -1,10 +1,24 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 
 
 class LIGFX:
-    def __init__(self, input_data_filename):
+    def __init__(self, input_data_filename, normalise=True):
         self.input_data = self.__read_input(input_data_filename)
-        self.norm_data = self.__normalise()
+        if normalise:
+            self.norm_input_data = self.__normalise()
+            self.input_x = self.norm_input_data.drop('y', axis=1)
+            self.input_y = self.norm_input_data['y']
+        else:
+            self.input_x = self.input_data.drop('y', axis=1)
+            self.input_y = self.input_data['y']
+        self.training_x = None
+        self.training_y = None
+        self.test_x = None
+        self.test_y = None
+        self.predicted_y = None
+        self.classifier = None
 
     @staticmethod
     def __read_input(input_data_filename):
@@ -14,3 +28,27 @@ class LIGFX:
     def __normalise(self):
         norm_data = (self.input_data - self.input_data.min()) / (self.input_data.max() - self.input_data.min())
         return norm_data
+
+    def holdout(self, test_percentage=30):
+        x_training, x_test, y_training, y_test = train_test_split(
+            self.input_x, self.input_y, test_size=(test_percentage/100), random_state=0)
+        self.training_x = x_training
+        self.training_y = y_training
+        self.test_x = x_test
+        self.test_y = y_test
+
+    def create_classifier(self, method=LogisticRegression()):
+        self.classifier = method
+
+    def run_training(self):
+        if (self.training_x is None) or (self.training_y is None):
+            print("Warning: training set not defined.")
+            return 1
+        if self.classifier is None:
+            print("Warning: machine learning method not defined.")
+            return 1
+        self.classifier.fit(self.training_x, self.training_y)
+
+    def run_prediction(self):
+        self.predicted_y = self.classifier.predict(self.test_x)
+
