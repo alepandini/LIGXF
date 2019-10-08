c406561fb885e5c7ff2fad2f58bdbb714cfdaf8a
Alessandro Pandini <alessandro.pandini@gmail.com>
2019-10-08 15:58:36 +0100

    add normalisation test

--- LIGFX_test.py
+++ LIGFX_test.py
@@ -5,10 +5,15 @@ from LIGFX import *
 class TestLIGFX(unittest.TestCase):
 
     def test_constructor(self):
-        ligfx_object = LIGFX("test/test001_input.csv", normalise=True)
+        ligfx_object = LIGFX("test/test001_input.csv")
         dataset_dim = (133, 28)
         self.assertEqual(ligfx_object.input_x.shape, dataset_dim,
                          "Dataset size should [%d,%d]" % (dataset_dim[0], dataset_dim[1]))
+
+    def test_normalisation(self):
+        ligfx_object = LIGFX("test/test001_input.csv", normalise=True)
+        data_point = 0.5258266956311851
+        self.assertEqual(ligfx_object.input_x.iloc[0][0], data_point, "Dataset not correctly normalised")
 
 
 if __name__ == '__main__':
