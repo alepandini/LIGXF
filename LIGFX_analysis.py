d605f2a0e3e65659448f59f47f01e8b7e09055d5
Alessandro Pandini <alessandro.pandini@gmail.com>
2021-03-31 17:21:42 +0100

    add GPL copyright statemente

--- LIGFX_analysis.py
+++ LIGFX_analysis.py
@@ -1,3 +1,26 @@
+#!/usr/bin/python3
+'''
+    @release_date  : $release_date
+    @version       : $release_version
+    @author        : Filippo Marchetti
+
+    
+     This file is part of the LIGFX distribution (https://github.com/alepandini/LIGXF).
+     Copyright (c) 2020-21 Filippo Marchetti, Giorgio Colombo and Alessandro Pandini.
+
+     This program is free software: you can redistribute it and/or modify 
+     it under the terms of the GNU General Public License as published by  
+     the Free Software Foundation, version 3.
+
+     This program is distributed in the hope that it will be useful, but 
+     WITHOUT ANY WARRANTY; without even the implied warranty of 
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+     General Public License for more details.
+
+     You should have received a copy of the GNU General Public License 
+     along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+'''
 import os
 import argparse
 from LIGFX import *
